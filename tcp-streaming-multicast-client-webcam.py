9ab7228842e5ead6fdd4772aa523a5d6b4ed0a1a
Nicolás Batistoni <nicolasbatistoni@gmail.com>
2018-02-07 11:30:33 -0300

    wip

--- tcp-streaming-multicast-client-webcam.py
+++ tcp-streaming-multicast-client-webcam.py
@@ -22,7 +22,7 @@ __python_version__ = "2.7"
 __email__ = "jagercox@gmail.com"
 
 IP_SERVER = "0.0.0.0"
-PORT_SERVER = 953
+PORT_SERVER = 1025
 TIMEOUT_SOCKET = 10
 SIZE_PACKAGE = 4096
 
@@ -30,6 +30,7 @@ IMAGE_HEIGHT = 480
 IMAGE_WIDTH = 640
 COLOR_PIXEL = 3  # RGB
 
+DEVICE_NUMBER = 0
 
 if __name__ == '__main__':
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -37,23 +38,17 @@ if __name__ == '__main__':
     connection.settimeout(TIMEOUT_SOCKET)
     connection.connect((IP_SERVER, PORT_SERVER))
 
+    camera = cv2.VideoCapture(DEVICE_NUMBER)
+
     while True:
         try:
-            fileDescriptor = connection.makefile(mode='rb')
-            result = fileDescriptor.readline()
-            fileDescriptor.close()
-            result = base64.b64decode(result)
+            ret, frame = camera.read()
+            if ret:
+                data = frame.tostring()
+                connection.sendall(base64.b64encode(data) + '\r\n')
 
-            frame = np.fromstring(result, dtype=np.uint8)
-            frame_matrix = np.array(frame)
-            frame_matrix = np.reshape(frame_matrix, (IMAGE_HEIGHT, IMAGE_WIDTH,
-                                                     COLOR_PIXEL))
-            cv2.imshow('Window title', frame_matrix)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
 
         except Exception as e:
             print "[Error] " + str(e)
 
-    connection.close()
+connection.close()
--- tcp-streaming-multicast-server-webcam.py
+++ tcp-streaming-multicast-server-webcam.py
@@ -25,35 +25,47 @@ __python_version__ = "2.7"
 __email__ = "jagercox@gmail.com"
 
 SERVER_IP = "0.0.0.0"
-SERVER_PORT = 953
+SERVER_PORT = 1025
 MAX_NUM_CONNECTIONS = 20
-DEVICE_NUMBER = 1
+DEVICE_NUMBER = 0
+
+IMAGE_HEIGHT = 480
+IMAGE_WIDTH = 640
+COLOR_PIXEL = 3  # RGB
 
 
 class ConnectionPool(Thread):
 
-    def __init__(self, ip_, port_, conn_, device_):
+    def __init__(self, ip_, port_, conn_):
         Thread.__init__(self)
         self.ip = ip_
         self.port = port_
         self.conn = conn_
-        self.device = device_
         print "[+] New server socket thread started for " + self.ip + ":" + \
             str(self.port)
 
     def run(self):
         try:
             while True:
-                ret, frame = self.device.read()
-                data = frame.tostring()
-                self.conn.sendall(base64.b64encode(data) + '\r\n')
+                fileDescriptor = self.conn.makefile(mode='rb')
+                result = fileDescriptor.readline()
+                fileDescriptor.close()
+                result = base64.b64decode(result)
+
+                frame = np.fromstring(result, dtype=np.uint8)
+                frame_matrix = np.array(frame)
+                frame_matrix = np.reshape(frame_matrix, (IMAGE_HEIGHT, IMAGE_WIDTH,
+                                                         COLOR_PIXEL))
+                cv2.imshow('Window title', frame_matrix)
+
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
         except Exception, e:
             print "Connection lost with " + self.ip + ":" + str(self.port) + \
                   "\r\n[Error] " + str(e.message)
         self.conn.close()
 
 if __name__ == '__main__':
-    camera = cv2.VideoCapture(DEVICE_NUMBER)
     print "Waiting connections..."
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -61,7 +73,7 @@ if __name__ == '__main__':
     connection.listen(MAX_NUM_CONNECTIONS)
     while True:
         (conn, (ip, port)) = connection.accept()
-        thread = ConnectionPool(ip, port, conn, camera)
+        thread = ConnectionPool(ip, port, conn)
         thread.start()
     connection.close()
-    camera.release()
+camera.release()
